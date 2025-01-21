cbb3acf6f2a104d21c42f36644aab8233aff2114
sufyan20033 <sufyanlehri10@gmail.com>
2025-01-21 21:52:51 +0500

    .

--- app/(root)/tabs/profile.tsx
+++ app/(root)/tabs/profile.tsx
@@ -1,16 +1,17 @@
-import { View, Text, ScrollView,Image, Touchable, TouchableOpacity, ImageSourcePropType } from 'react-native'
+import { View, Text, ScrollView, Image, Touchable, TouchableOpacity, ImageSourcePropType } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import icons from '@/constants/icons';
 import images from '@/constants/images';
 import { settings } from '@/constants/data';
+import { logout } from '@/app/lib/appwrite';
 
-interface SettingsItemsProps{
+interface SettingsItemsProps {
   icon: ImageSourcePropType;
   title: String;
-  onPress:() => void;
-  textStyle ? :String;
-  ShowArrrow ?:boolean;
+  onPress: () => void;
+  textStyle?: String;
+  ShowArrrow?: boolean;
 }
 
 
@@ -23,53 +24,62 @@ const SettingItems = ({ icon, title, onPress, textStyle, ShowArrrow = true }: Se
           {title}
         </Text>
       </View>
-      {ShowArrrow && <Image className='size-6 mt-9' source={icons.rightArrow}/>}
+      {ShowArrrow && <Image className='size-6 mt-9' source={icons.rightArrow} />}
     </TouchableOpacity>
   )
 }
-const handlebooking = ()=>{
+const handlebooking = () => {
 
 }
 const profile = () => {
-  return (
-    <SafeAreaView className='h-full bg-white'> 
-      <ScrollView showsVerticalScrollIndicator={false}
+
+
+
+
+return (
+  <SafeAreaView className='h-full bg-white'>
+    <ScrollView showsVerticalScrollIndicator={false}
       contentContainerClassName='pb-32 px-7'
-      >
-        <View className='flex flex-row items-center justify-between mt-5'>
-          <Text className='text-2xl font-rubik-bold'>
-            Profile
+    >
+      <View className='flex flex-row items-center justify-between mt-5'>
+        <Text className='text-2xl font-rubik-bold'>
+          Profile
+        </Text>
+        <Image source={icons.bell} className='size-5' />
+      </View>
+
+      <View className=' flex flex-row justify-center mt-5'>
+        <View className='flex flex-col items-center relative mt-5'>
+          <Image source={{uri : images?.avatar}} className='size-44 relative rounded-full' />
+          <TouchableOpacity className='absolute bottom-11 right-1'>
+            <Image source={icons.edit} className='size-9' />
+          </TouchableOpacity>
+          <Text className='font-rubik-bold text-2xl mt-2'>
+            sufyan
           </Text>
-            <Image source={icons.bell} className='size-5'/>
         </View>
+      </View>
+      <View className='flex flex-col'>
+        <SettingItems icon={icons.calendar} title='Bookings' onPress={handlebooking} />
+        <SettingItems icon={icons.wallet} title='Payments' onPress={handlebooking} />
 
-        <View className=' flex flex-row justify-center mt-5'>
-          <View className='flex flex-col items-center relative mt-5'> 
-              <Image source={images.avatar} className='size-44 relative rounded-full'/>
-              <TouchableOpacity className='absolute bottom-11 right-1'>
-                <Image source={icons.edit} className='size-9'/>
-              </TouchableOpacity>
-              <Text className='font-rubik-bold text-2xl mt-2'>
-                sufyan
-              </Text>
-          </View>
-        </View>
-        <View className='flex flex-col'>
-            <SettingItems icon={icons.calendar} title='Bookings' onPress={handlebooking}/>
-            <SettingItems icon={icons.wallet} title='Payments' onPress={handlebooking}/>
-        </View>
+      </View>
 
 
-        {/* <View className='flex flex-col mt-10'>
-            {settings.slice(2).map((item, index)=>{
-              <SettingItems key={index} {...item}/>
-            })}
-        </View> */}
+      <View className="flex flex-col mt-10 border-t pt-5 border-primary-200">
+        {settings.slice(2).map((item, index) => (
+          <SettingItems key={index} {...item} onPress={handlebooking} />
+        ))}
+      </View>
+
+      <View className='flex flex-col  mt-10 border-t pt-5 border-primary-200'>
+        <SettingItems icon={icons.logout} title='Logout' textStyle='text-danger' ShowArrrow={false} onPress={handlebooking} />
+
+      </View>
 
 
-      </ScrollView>
-    </SafeAreaView>
-  )
+    </ScrollView>
+  </SafeAreaView>
+)
 }
-
 export default profile;
